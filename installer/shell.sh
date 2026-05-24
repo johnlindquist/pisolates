b45bb69035bcb6af488b01bc5a3ed671525ac555
John Lindquist <johnlindquist@gmail.com>
2026-05-23 22:38:27 -0600

    Install pisolates shell path by default

--- installer/shell.sh
+++ installer/shell.sh
@@ -2,6 +2,6 @@
 set -eu
 home=${PISOLATES_HOME:-$HOME/.pisolates}
 mkdir -p "$home/shell"
-printf 'export PATH="$HOME/.pisolates/bin:$PATH"\n' > "$home/shell/env.sh"
-printf 'export PATH="$HOME/.pisolates/bin:$PATH"\n' > "$home/shell/env.zsh"
+printf 'export PATH="%s/bin:$PATH"\n' "$home" > "$home/shell/env.sh"
+printf 'export PATH="%s/bin:$PATH"\n' "$home" > "$home/shell/env.zsh"
 echo "$home/shell/env.zsh"
